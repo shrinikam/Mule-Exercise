7ad9fa96a571c6c0ea9c04729635b4f655168777
shrinikam <76509188+shrinikam@users.noreply.github.com>
2022-04-07 00:53:44 +0530

    Add files via upload

---
+++ dw-tutorial4-flight-ws/src/main/java/com/flight/ws/ui/model/request/flightDetailsRequestModel.java
@@ -0,0 +1,70 @@
+package com.flight.ws.ui.model.request;
+
+public class flightDetailsRequestModel {
+	private String ID;
+	private String code;
+	private String price	;
+	private String departureDate;
+	private String origin;
+	private String destination;
+	private String emptySeats;
+	private String planeType;
+	private String totalSeats;
+	
+	
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getDepartureDate() {
+		return departureDate;
+	}
+	public void setDepartureDate(String departureDate) {
+		this.departureDate = departureDate;
+	}
+	public String getOrigin() {
+		return origin;
+	}
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public String getEmptySeats() {
+		return emptySeats;
+	}
+	public void setEmptySeats(String emptySeats) {
+		this.emptySeats = emptySeats;
+	}
+	public String getPlaneType() {
+		return planeType;
+	}
+	public void setPlaneType(String planeType) {
+		this.planeType = planeType;
+	}
+	public String getTotalSeats() {
+		return totalSeats;
+	}
+	public void setTotalSeats(String totalSeats) {
+		this.totalSeats = totalSeats;
+	}
+
+}
